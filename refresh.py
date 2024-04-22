dc7fae61293877c6a3bc8c9ec78e381531b44e05
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2024-04-22 14:06:33 +0300

    Testing

--- refresh.py
+++ refresh.py
@@ -184,6 +184,7 @@ for x in whitelist.keys():
             # Categorize all domains under API or CDN groups
             if z["Type"] == "API" or z["Type"] == "CDN":
                 if z["Type"] not in Doms.keys():
+                    print(z["Domain"])
                     Doms[z["Type"]] = {z["Comment"]: [z["Domain"]]}
                 if z["Comment"] not in Doms[z["Type"]].keys():
                     Doms[z["Type"]][z["Comment"]] = [z["Domain"]]
