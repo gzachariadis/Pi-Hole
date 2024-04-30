b1c1f3f7f0add25a3957d1b5a89b2756feadb255
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2024-04-30 14:55:58 +0300

    Testing

--- refresh.py
+++ refresh.py
@@ -141,7 +141,7 @@ try:
 except subprocess.CalledProcessError as cpe:
     result = cpe.output
 
-# print(json.dumps(whitelist, sort_keys=False, indent=4))
+print(json.dumps(whitelist, sort_keys=False, indent=4))
 
 """
 # Reset the Structure before re-creating
