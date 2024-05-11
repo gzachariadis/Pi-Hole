172dc48e8e26cebc46921e4d53390f06b3ccd447
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2024-05-11 14:09:16 +0300

    Adding

--- export.py
+++ export.py
@@ -21,10 +21,17 @@ try:
         black_command, shell=True, executable="/bin/bash", stderr=subprocess.STDOUT
     )
 
+    # Create a New File (named Number-Date) inside the last Iteration
+    # Append to it line by line a formatted domain
+    # Count the lines you write, every 10.000 lines
+    # Save File - Switch to a new File (reset loop)
+    # When input is complete nuke the exact blacklist
+    # Verify the exact blacklist is nuked.
+    
     for line in blacks.splitlines():
         # Fetch Data by line
         line = str(line.decode()).rstrip().strip()
-        print(line)
+    
 
     
 except subprocess.CalledProcessError as cpe:
