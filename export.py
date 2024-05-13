40f8d54422a1bebffc53e189ec4fd7fcba42e18d
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2024-05-13 13:26:35 +0300

    Adding

--- export.py
+++ export.py
@@ -53,7 +53,7 @@ def return_subDirs(subdirectory):
     return [f.name for f in os.scandir(subdirectory) if f.is_dir()]
 
 def create_path(folder,file):
-    return (os.path.abspath(os.path.dirname(folder)) + "/" + file.replace(" ", "\ "))
+    # return (os.path.abspath(os.path.dirname(folder)) + "/" + file.replace(" ", "\ "))
     print((os.path.abspath(os.path.dirname(folder))))
     print(folder)
     print(file)
