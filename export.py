105b3905d9486fd576de119968fc5cf0107490ae
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2024-05-13 15:22:14 +0300

    Adding

--- export.py
+++ export.py
@@ -39,34 +39,39 @@ try:
     blacks = subprocess.check_output(
         black_command, shell=True, executable="/bin/bash", stderr=subprocess.STDOUT
     )
-
-    # Project Root Directory
-    root_directory = dir_path = os.path.dirname(os.path.realpath(__file__))
     
-    # Set Search onto Adlists > Sources
-    subD = os.path.join(root_directory, "Blacklist", "Adlists","Sources")
-    
-    # Fetch all Subfolders inside Adlists > Sources
-    subF = return_subDirs(subdirectory=subD)
-    
-    # Sort them so you add to the last One
-    selected = sorted(subF)[-1]
-    
-    # Change Working Directory to Last Source
-    last_iteration = create_path(subD,str(selected))
-    os.chdir(Path(last_iteration))
-
-    # Step 2 : Get all Txt Files in Current Directory
-    TextFiles = [f for f in os.listdir(os.curdir) if f.endswith('.txt')]
-    today = date.today().strftime("%d-%m-%Y")
-    counter = count(Date=today,Files=TextFiles) 
-   
-    # Step 3 : Create the File based on BlackDomains (Backup)
-   
-    # Check if Bytes are Empty - So no domains
     if not blacks:
+    
+        print("Blacklist is Empty...")
         sys.exit()
+    
     else:
+        
+        # Project Root Directory
+        root_directory = dir_path = os.path.dirname(os.path.realpath(__file__))
+        
+        # Set Search onto Adlists > Sources
+        subD = os.path.join(root_directory, "Blacklist", "Adlists","Sources")
+        
+        # Fetch all Subfolders inside Adlists > Sources
+        subF = return_subDirs(subdirectory=subD)
+        
+        # Sort them so you add to the last One
+        selected = sorted(subF)[-1]
+        
+        # Change Working Directory to Last Source
+        last_iteration = create_path(subD,str(selected))
+        os.chdir(Path(last_iteration))
+
+        # Step 2 : Get all Txt Files in Current Directory
+        TextFiles = [f for f in os.listdir(os.curdir) if f.endswith('.txt')]
+        today = date.today().strftime("%d-%m-%Y")
+        counter = count(Date=today,Files=TextFiles) 
+    
+        # Step 3 : Create the File based on BlackDomains (Backup)
+    
+        # Check if Bytes are Empty - So no domains
+
         # Create a New Text File (named [Number]. Date inside the folder
         with open('.'.join([str("{:02d}".format(counter)),str(today),'txt']), 'w') as f:
             lines = 0 
@@ -79,7 +84,7 @@ try:
                 lines  = lines + 1
             # Save the File
             f.close()
-            
+                
     # Step 4 - Delete Exact Blacklist 
 
     delete_command = " ".join(
@@ -93,18 +98,18 @@ try:
         'type=1;"',
     ])
     
-    # If code processed blacklist 
+    # In case blacklist commands is processed...
     if int(lines) > 0:
         # Verify the exact blacklist is nuked.
         time.sleep(3)
         # Delete Exact Blacklist using PiHole Command
+        print("Nuking Blacklist using Pi-Hole Interface Command...")
         subprocess.call(["pihole", "-b", "--nuke"]) 
         time.sleep(5)
+        print("Nuking Blacklist using Database Command...")
         # Nuke the exact blacklist by using the delete_command (Directly from Database)
         subprocess.call(delete_command,shell=True, executable="/bin/bash")
         
 except subprocess.CalledProcessError as cpe:
     blacks = cpe.output
-    
-    
-
+    print("Error when extracting blacklist...")
