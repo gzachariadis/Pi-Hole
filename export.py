83192bbd0e680c800f98a178d4955e1ae9000987
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2024-05-13 12:44:16 +0300

    Adding

--- export.py
+++ export.py
@@ -16,24 +16,57 @@ black_command = " ".join(
     ]
 )
 
+# sudo sqlite3 /etc/pihole/gravity.db "delete from domainlist where type=1;"
+
+delete_command = " ".join(
+    [
+        "sqlite3",
+        '"/etc/pihole/gravity.db"',
+        '"DELETE',
+        "FROM",
+        "domainlist",
+        "WHERE",
+        'type=1;"',
+    ]
+)
+
 try:
     blacks = subprocess.check_output(
         black_command, shell=True, executable="/bin/bash", stderr=subprocess.STDOUT
     )
 
-    # Create a New File (named Number-Date) inside the last Iteration
-    # Append to it line by line a formatted domain
+    # Fetch all Subfolders inside Adlists > Sources
+    root_directory = dir_path = os.path.dirname(os.path.realpath(__file__))
+    
+    print(root_directory)
+    
+    
+    # Find the Last One, cd to it
+    
+    # Fetch all Text Files inside of it.
+    
+    # Create a New Text File (named [Number]. Date inside the folder
+    
+    # Append to it line by line a formatted domain entry.
+    
     # Count the lines you write, every 10.000 lines
     # Save File - Switch to a new File (reset loop)
-    # When input is complete nuke the exact blacklist
+    # When input is complete 
+    # Nuke the exact blacklist by using the delete_command
     # Verify the exact blacklist is nuked.
     
+    ## pihole -b --nuke ---> Another way to delete
+    
+    ## pihole -b --list ---> Must be "Not showing empty list"
+    
+    """
     for line in blacks.splitlines():
         # Fetch Data by line
         line = str(line.decode()).rstrip().strip()
-    
+    """
 
     
 except subprocess.CalledProcessError as cpe:
     blacks = cpe.output
-    print(blacks)
+    
+    
